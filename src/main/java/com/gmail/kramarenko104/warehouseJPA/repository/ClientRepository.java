f3ef6f64c185df1f73a385525f0b5a0e389b6a7b
Iuliia Kramarenko <juli127@bigmir.net>
2018-11-17 12:47:28 +0200

    simplify mapping annotation for controllers

--- src/main/java/com/gmail/kramarenko104/warehouseJPA/repository/ClientRepository.java
+++ src/main/java/com/gmail/kramarenko104/warehouseJPA/repository/ClientRepository.java
@@ -1,7 +1,6 @@
 package com.gmail.kramarenko104.warehouseJPA.repository;
 
 import com.gmail.kramarenko104.warehouseJPA.model.Client;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
